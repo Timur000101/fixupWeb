c7ac9d375b597a192dda5daf7eac5f0eef9753bf
Temur <tima000101@gmail.com>
2020-03-31 14:19:28 +0600

    Deploy to Fix-up.org

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,206 +28,206 @@ export default {
   data: () => ({
     
   }),
-  // created() {
-  //   window.onload = function() {
-  //     // First slide constants
-  //     const WOMAN = document.querySelector("#woman")
-  //     const COMPUTER = document.querySelector('#computer')
-  //     const SPARK = document.querySelector('#spark')
-  //     const SMOKE = document.querySelector('#smoke')
-  //     // Second slide constants
-  //     const MAN = document.querySelector("#man")
-  //     const STAIRS = document.querySelector("#stairs")
-  //     const PICTURE = document.querySelector("#picture")
-  //     const CLOCK = document.querySelector("#clock")
-  //     // Third slide constants
-  //     const MAN_LOCATION = document.querySelector("#man_location")
-  //     const WOMAN_LOCATION = document.querySelector("#woman_location")
-  //     const ROAD = document.querySelector("#road")
-  //     // Forth slide constants
-  //     const MAN_WOMAN = document.querySelector("#man_woman")
-  //     const BOOKS = document.querySelector("#books")
-  //     const COMP_TABLE = document.querySelector("#comp_table")
+  created() {
+    window.onload = function() {
+      // First slide constants
+      const WOMAN = document.querySelector("#woman")
+      const COMPUTER = document.querySelector('#computer')
+      const SPARK = document.querySelector('#spark')
+      const SMOKE = document.querySelector('#smoke')
+      // Second slide constants
+      const MAN = document.querySelector("#man")
+      const STAIRS = document.querySelector("#stairs")
+      const PICTURE = document.querySelector("#picture")
+      const CLOCK = document.querySelector("#clock")
+      // Third slide constants
+      const MAN_LOCATION = document.querySelector("#man_location")
+      const WOMAN_LOCATION = document.querySelector("#woman_location")
+      const ROAD = document.querySelector("#road")
+      // Forth slide constants
+      const MAN_WOMAN = document.querySelector("#man_woman")
+      const BOOKS = document.querySelector("#books")
+      const COMP_TABLE = document.querySelector("#comp_table")
 
-  //     // First slide listeners
-  //     window.addEventListener('scroll', scrollWoman)
-  //     window.addEventListener('scroll', scrollComputer)
-  //     window.addEventListener('scroll', scrollSpark)
-  //     window.addEventListener('scroll', scrollSmoke)
-  //     // Second slide listeners
-  //     window.addEventListener('scroll', scrollMan)
-  //     window.addEventListener('scroll', scrollStairs)
-  //     window.addEventListener('scroll', scrollPicture)
-  //     window.addEventListener('scroll', scrollClock)
-  //     // Third slide listeners
-  //     window.addEventListener('scroll', scrollManlocation)
-  //     window.addEventListener('scroll', scrollWomanlocation)
-  //     window.addEventListener('scroll', scrollRoad) 
-  //     // Forth slide listeners 
-  //     window.addEventListener('scroll', scrollManwoman)
-  //     window.addEventListener('scroll', scrollBooks)
-  //     window.addEventListener('scroll', scrollComptable)
+      // First slide listeners
+      window.addEventListener('scroll', scrollWoman)
+      window.addEventListener('scroll', scrollComputer)
+      window.addEventListener('scroll', scrollSpark)
+      window.addEventListener('scroll', scrollSmoke)
+      // Second slide listeners
+      window.addEventListener('scroll', scrollMan)
+      window.addEventListener('scroll', scrollStairs)
+      window.addEventListener('scroll', scrollPicture)
+      window.addEventListener('scroll', scrollClock)
+      // Third slide listeners
+      window.addEventListener('scroll', scrollManlocation)
+      window.addEventListener('scroll', scrollWomanlocation)
+      window.addEventListener('scroll', scrollRoad) 
+      // Forth slide listeners 
+      window.addEventListener('scroll', scrollManwoman)
+      window.addEventListener('scroll', scrollBooks)
+      window.addEventListener('scroll', scrollComptable)
 
-  //     // First slide illustrations
-  //     // Scroll computer table animation
-  //     function scrollComputer() {
-  //       if(window.scrollY >= 650) {
-  //         COMPUTER.style.opacity = '1'
-  //         COMPUTER.style.transition = '0.9s ease-in-out'
-  //         COMPUTER.style.transform = 'translateX(0px)'
-  //       } else {
-  //         COMPUTER.style.opacity = '0'
-  //         COMPUTER.style.transform = 'translateX(100px)'
-  //       }
-  //     }
-  //     // Scroll woman animation
-  //     function scrollWoman() {
-  //       if(window.scrollY >= 550) {
-  //         WOMAN.style.opacity = '1'
-  //         WOMAN.style.transform = 'translateX(0px)'
-  //         WOMAN.style.transition = '0.9s ease-in-out'
-  //       } else {
-  //         WOMAN.style.opacity = '0'
-  //         WOMAN.style.transform = 'translateX(200px)'
-  //       }
-  //     }
-  //     // Scroll spark animation
-  //     function scrollSpark() {
-  //       if(window.scrollY >= 750) {
-  //         SPARK.style.opacity = '1'
-  //         SPARK.style.transition = '1s ease-in-out'
-  //       } else {
-  //         SPARK.style.opacity = '0'
-  //       }
-  //     }
-  //     // Scroll smoke animation
-  //     function scrollSmoke() {
-  //       if(window.scrollY >= 850) {
-  //         SMOKE.style.opacity = '1'
-  //         SMOKE.style.transform = 'translateY(0px)'
-  //         SMOKE.style.transition = '0.9s ease-in-out'
-  //       } else {
-  //         SMOKE.style.opacity = '0'
-  //         SMOKE.style.transform = 'translateY(50px)'
-  //       }
-  //     }
-
-
-  //     // Second slide illustrations
-  //     function scrollMan() {
-  //       if(window.scrollY >= 1200) {
-  //         MAN.style.opacity = '1'
-  //         MAN.style.transition = '0.8s ease-in-out'
-  //         MAN.style.transform = 'translateX(0px)'
-  //       } else {
-  //         MAN.style.opacity = '0'
-  //         MAN.style.transform = 'translateX(-100px)'
-  //       }
-  //     }
-
-  //     function scrollStairs() {
-  //       if(window.scrollY >= 1250) {
-  //         STAIRS.style.opacity = '1'
-  //         STAIRS.style.transition = '0.8s ease-in-out'
-  //         STAIRS.style.transform = 'translateX(0px)'
-  //       } else {
-  //         STAIRS.style.opacity = '0'
-  //         STAIRS.style.transform = 'translateX(-100px)'
-  //       }
-  //     }
-
-  //     function scrollPicture() {
-  //       if(window.scrollY >= 1300) {
-  //         PICTURE.style.opacity = '1'
-  //         PICTURE.style.transition = '0.6s ease-in-out'
-  //         PICTURE.style.transform = 'translateX(0px)'
-  //       } else {
-  //         PICTURE.style.opacity = '0'
-  //         PICTURE.style.transform = 'translateX(-50px)'
-  //       }
-  //     }
-
-  //     function scrollClock() {
-  //       if(window.scrollY >= 1300) {
-  //         CLOCK.style.opacity = '1'
-  //         CLOCK.style.transition = '0.6s ease-in-out'
-  //         CLOCK.style.transform = 'translateX(0px)'
-  //       } else {
-  //         CLOCK.style.opacity = '0'
-  //         CLOCK.style.transform = 'translateX(-50px)'
-  //       }
-  //     }
+      // First slide illustrations
+      // Scroll computer table animation
+      function scrollComputer() {
+        if(window.scrollY >= 650) {
+          COMPUTER.style.opacity = '1'
+          COMPUTER.style.transition = '0.9s ease-in-out'
+          COMPUTER.style.transform = 'translateX(0px)'
+        } else {
+          COMPUTER.style.opacity = '0'
+          COMPUTER.style.transform = 'translateX(100px)'
+        }
+      }
+      // Scroll woman animation
+      function scrollWoman() {
+        if(window.scrollY >= 550) {
+          WOMAN.style.opacity = '1'
+          WOMAN.style.transform = 'translateX(0px)'
+          WOMAN.style.transition = '0.9s ease-in-out'
+        } else {
+          WOMAN.style.opacity = '0'
+          WOMAN.style.transform = 'translateX(200px)'
+        }
+      }
+      // Scroll spark animation
+      function scrollSpark() {
+        if(window.scrollY >= 750) {
+          SPARK.style.opacity = '1'
+          SPARK.style.transition = '1s ease-in-out'
+        } else {
+          SPARK.style.opacity = '0'
+        }
+      }
+      // Scroll smoke animation
+      function scrollSmoke() {
+        if(window.scrollY >= 850) {
+          SMOKE.style.opacity = '1'
+          SMOKE.style.transform = 'translateY(0px)'
+          SMOKE.style.transition = '0.9s ease-in-out'
+        } else {
+          SMOKE.style.opacity = '0'
+          SMOKE.style.transform = 'translateY(50px)'
+        }
+      }
 
 
-  //     // Third slide illustrations
-  //     function scrollManlocation() {
-  //       if(window.scrollY >= 2000) {
-  //         MAN_LOCATION.style.opacity = '1'
-  //         MAN_LOCATION.style.transition = '0.6s ease-in-out'
-  //         MAN_LOCATION.style.transform = 'translateX(0px)'
-  //       } else {
-  //         MAN_LOCATION.style.opacity = '0'
-  //         MAN_LOCATION.style.transform = 'translateX(-50px)'
-  //       }
-  //     }
+      // Second slide illustrations
+      function scrollMan() {
+        if(window.scrollY >= 1200) {
+          MAN.style.opacity = '1'
+          MAN.style.transition = '0.8s ease-in-out'
+          MAN.style.transform = 'translateX(0px)'
+        } else {
+          MAN.style.opacity = '0'
+          MAN.style.transform = 'translateX(-100px)'
+        }
+      }
 
-  //     function scrollRoad() {
-  //       if(window.scrollY >= 2150) {
-  //         ROAD.style.opacity = '1'
-  //         ROAD.style.transition = '0.8s ease-in-out'
-  //         ROAD.style.transform = 'translateX(0px)'
-  //       } else {
-  //         ROAD.style.opacity = '0'
-  //         ROAD.style.transform = 'translateX(-50px)'
-  //       }
-  //     }
+      function scrollStairs() {
+        if(window.scrollY >= 1250) {
+          STAIRS.style.opacity = '1'
+          STAIRS.style.transition = '0.8s ease-in-out'
+          STAIRS.style.transform = 'translateX(0px)'
+        } else {
+          STAIRS.style.opacity = '0'
+          STAIRS.style.transform = 'translateX(-100px)'
+        }
+      }
 
-  //     function scrollWomanlocation() {
-  //       if(window.scrollY >= 2250) {
-  //         WOMAN_LOCATION.style.opacity = '1'
-  //         WOMAN_LOCATION.style.transition = '0.8s ease-in-out'
-  //         WOMAN_LOCATION.style.transform = 'translateX(0px)'
-  //       } else {
-  //         WOMAN_LOCATION.style.opacity = '0'
-  //         WOMAN_LOCATION.style.transform = 'translateX(100px)'
-  //       }
-  //     }
+      function scrollPicture() {
+        if(window.scrollY >= 1300) {
+          PICTURE.style.opacity = '1'
+          PICTURE.style.transition = '0.6s ease-in-out'
+          PICTURE.style.transform = 'translateX(0px)'
+        } else {
+          PICTURE.style.opacity = '0'
+          PICTURE.style.transform = 'translateX(-50px)'
+        }
+      }
 
-  //     // Forth slide illustrations
-  //     function scrollBooks() {
-  //       if(window.scrollY >= 2700) {
-  //         BOOKS.style.opacity = '1'
-  //         BOOKS.style.transition = '0.9s ease-in-out'
-  //         BOOKS.style.transform = 'translateX(0px)'
-  //       } else {
-  //         BOOKS.style.opacity = '0'
-  //         BOOKS.style.transform = 'translateX(-100px)'
-  //       }
-  //     }
+      function scrollClock() {
+        if(window.scrollY >= 1300) {
+          CLOCK.style.opacity = '1'
+          CLOCK.style.transition = '0.6s ease-in-out'
+          CLOCK.style.transform = 'translateX(0px)'
+        } else {
+          CLOCK.style.opacity = '0'
+          CLOCK.style.transform = 'translateX(-50px)'
+        }
+      }
 
-  //     function scrollComptable() {
-  //       if(window.scrollY >= 2750) {
-  //         COMP_TABLE.style.opacity = '1'
-  //         COMP_TABLE.style.transition = '1s ease-in-out'
-  //         COMP_TABLE.style.transform = 'translateX(0px)'
-  //       } else {
-  //         COMP_TABLE.style.opacity = '0'
-  //         COMP_TABLE.style.transform = 'translateX(-100px)'
-  //       }
-  //     }
 
-  //     function scrollManwoman() {
-  //       if(window.scrollY >= 2800) {
-  //         MAN_WOMAN.style.opacity = '1'
-  //         MAN_WOMAN.style.transition = '0.9s ease-in-out'
-  //         MAN_WOMAN.style.transform = 'translateX(0px)'
-  //       } else {
-  //         MAN_WOMAN.style.opacity = '0'
-  //         MAN_WOMAN.style.transform = 'translateX(-100px)'
-  //       }
-  //     }
-  //   }
-  // }
+      // Third slide illustrations
+      function scrollManlocation() {
+        if(window.scrollY >= 2000) {
+          MAN_LOCATION.style.opacity = '1'
+          MAN_LOCATION.style.transition = '0.6s ease-in-out'
+          MAN_LOCATION.style.transform = 'translateX(0px)'
+        } else {
+          MAN_LOCATION.style.opacity = '0'
+          MAN_LOCATION.style.transform = 'translateX(-50px)'
+        }
+      }
+
+      function scrollRoad() {
+        if(window.scrollY >= 2150) {
+          ROAD.style.opacity = '1'
+          ROAD.style.transition = '0.8s ease-in-out'
+          ROAD.style.transform = 'translateX(0px)'
+        } else {
+          ROAD.style.opacity = '0'
+          ROAD.style.transform = 'translateX(-50px)'
+        }
+      }
+
+      function scrollWomanlocation() {
+        if(window.scrollY >= 2250) {
+          WOMAN_LOCATION.style.opacity = '1'
+          WOMAN_LOCATION.style.transition = '0.8s ease-in-out'
+          WOMAN_LOCATION.style.transform = 'translateX(0px)'
+        } else {
+          WOMAN_LOCATION.style.opacity = '0'
+          WOMAN_LOCATION.style.transform = 'translateX(100px)'
+        }
+      }
+
+      // Forth slide illustrations
+      function scrollBooks() {
+        if(window.scrollY >= 2700) {
+          BOOKS.style.opacity = '1'
+          BOOKS.style.transition = '0.9s ease-in-out'
+          BOOKS.style.transform = 'translateX(0px)'
+        } else {
+          BOOKS.style.opacity = '0'
+          BOOKS.style.transform = 'translateX(-100px)'
+        }
+      }
+
+      function scrollComptable() {
+        if(window.scrollY >= 2750) {
+          COMP_TABLE.style.opacity = '1'
+          COMP_TABLE.style.transition = '1s ease-in-out'
+          COMP_TABLE.style.transform = 'translateX(0px)'
+        } else {
+          COMP_TABLE.style.opacity = '0'
+          COMP_TABLE.style.transform = 'translateX(-100px)'
+        }
+      }
+
+      function scrollManwoman() {
+        if(window.scrollY >= 2800) {
+          MAN_WOMAN.style.opacity = '1'
+          MAN_WOMAN.style.transition = '0.9s ease-in-out'
+          MAN_WOMAN.style.transform = 'translateX(0px)'
+        } else {
+          MAN_WOMAN.style.opacity = '0'
+          MAN_WOMAN.style.transform = 'translateX(-100px)'
+        }
+      }
+    }
+  }
 }
 </script>
 
